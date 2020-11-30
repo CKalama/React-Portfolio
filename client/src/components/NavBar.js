f55ad9d3a9f9de45a3762ca9246961049ad12f12
Chris Kalama <chriskalama96@gmail.com>
2020-11-29 19:06:21 -0600

    created a NavBar js and a pages folder and began creating Navbar, trying to use react-router-dom and Link tag to link pages across the HTML

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+
+//import {Link} from "react-router-dom";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+      flexGrow: 1
+  },
+  menuButton: {
+      marginRight: theme.spacing(2)
+  },
+  title: {
+      flexGrow: 1
+  }
+  }));
+
+export default function NavBar() {
+  const classes = useStyles();
+
+  return (
+      <div className={classes.root}>
+      <AppBar style={{ backgroundColor: "blue", padding:"20px" }} position="static">
+          <Toolbar>
+          <Typography style={{textAlign:"left"}}variant="h6" className={classes.title}>
+              My React Portfolio
+          </Typography>
+    
+          <Button
+
+            //   onClick={e => e.preventDefault()}
+              //href={}
+              color="inherit"
+          >
+              About
+          </Button>
+          
+          <Button color="inherit">Experience</Button>
+          <Button color="inherit">Contact</Button>
+          </Toolbar>
+      </AppBar>
+      </div>
+  );
+}
---
+++ client/src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Container from "@material-ui/core/Container"
+
+function About() {
+    return (
+        <Container>
+            This is a Test!
+        </Container>
+    )
+}
+
+export default About
